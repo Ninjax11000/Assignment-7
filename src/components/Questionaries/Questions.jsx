53173ce46414aa3f97529e656e30963f19d15005
Ninjax11000 <jamiljoy007@gmail.com>
2023-03-31 16:09:34 +0600

    added layout of blogs and sidebar

---
+++ src/components/Questionaries/Questions.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Questions = () => {
+    return (
+        <div>
+            <div>
+                <p>Questionaries</p>
+            </div>
+        </div>
+    );
+};
+
+export default Questions;
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <div>
+            <div className='px-2 bg-secondary card'>
+                <h1>Sidebar</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;

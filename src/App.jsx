79c115fa3251214b19888c96b559707bd741a558
Ninjax11000 <jamiljoy007@gmail.com>
2023-03-31 14:55:03 +0600

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,39 @@ function App() {
 
   return (
     <div className="App">
-      <h1>nav</h1>
-     <div class="row row-cols-1 row-cols-md-2">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid ">
+          <a class="navbar-brand fs-2 fw-bold" href="#">Knowledge Hunt</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div class="row row-cols-1 row-cols-md-2">
         <div class="col">
-            <h1>Blogs</h1>
+          <h1>Blogs</h1>
         </div>
         <div class="col">
-            <h1>Sidebar</h1>
+          <h1>Sidebar</h1>
         </div>
 
-    </div>
-    <div>
+      </div>
+      <div>
         <p>Questionaries</p>
-    </div>
+      </div>
     </div>
   )
 }

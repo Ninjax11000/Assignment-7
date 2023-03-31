fcda1bbee53352034e33aafbc91ae948d68ca1d0
Ninjax11000 <jamiljoy007@gmail.com>
2023-03-31 20:18:30 +0600

    added fontawesome logo

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,29 +1,11 @@
 import React from 'react';
+import Blog from '../Blog/Blog';
 import './Blogs.css'
 
 const Blogs = () => {
     return (
         <div>
-            <div className=' card bg-primary my-2 '>
-                <div >
-                    <img className='w-100 rounded' src="https://plus.unsplash.com/premium_photo-1663852297654-56d979cf72d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" />
-                </div>
-                <div className='d-flex align-items-center justify-content-between'>
-                    <div className='d-flex'>
-                    <img className='rounded-circle a-img' src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" />
-                    <div>
-                    <h3>Author</h3>
-                    <p>Mar 14(4days ago)</p>
-                    </div>
-                    </div>
-                    <p>10 min read</p>
-                </div>
-               
-                <h2>Floral decoration tips</h2>
-                
-            </div>
-            <button className='text-start'>Mark as Read</button>
-                <hr />
+           <Blog></Blog>
         </div>
     );
 };

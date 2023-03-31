b8403c1e3f71b574d46a49bc48969f7c4a0b8bcd
Ninjax11000 <jamiljoy007@gmail.com>
2023-03-31 21:03:34 +0600

    fixed card layout

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -6,25 +6,25 @@ const Blog = (props) => {
     console.log(props.blog.image);
     return (
         <div>
-            <div className=' card bg-primary my-2 '>
+            <div className=' card  my-2  '>
                 <div >
                     <img className='w-100 rounded' src={props.blog.image} alt="" />
                 </div>
-                <div className='d-flex align-items-center justify-content-between'>
-                    <div className='d-flex'>
+                <div className='d-flex align-items-center justify-content-between mt-3'>
+                    <div className='d-flex ms-2'>
                     <img className='rounded-circle a-img' src={props.blog.author_img} alt="" />
-                    <div>
-                    <h3>{props.blog.author}</h3>
-                    <p>Mar 14(4days ago)</p>
+                    <div className='ms-2'>
+                    <h3 className='fs-5 fw-bold'>{props.blog.author}</h3>
+                    <p className='text-secondary'>Mar 14(4days ago)</p>
                     </div>
                     </div>
-                    <p>{props.blog.read_time} min read <span><button><FontAwesomeIcon icon={faBookmark} /></button></span> </p>
+                    <p className='text-secondary'>{props.blog.read_time} min read <span><button className='btn btn-link'><FontAwesomeIcon icon={faBookmark} /></button></span> </p>
                 </div>
                
-                <h2>{props.blog.title}</h2>
-                
+                <h2 className='fs-2 fw-bold ms-2'>{props.blog.title}</h2>
+                <button className='text-start btn btn-link w-25 fw-bold'>Mark as Read </button>
             </div>
-            <button className='text-start'>Mark as Read </button>
+            
                 <hr /> 
         </div>
     );
